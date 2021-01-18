dd241c5ac21e1f876761497df8e90033e53b42ae
Abhinav Verma <21.abhinav@gmail.com>
2021-01-18 10:21:49 +0000

    Initial Push

---
+++ webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"./BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("abhi.Znorthwindodata1.controller.DetailObjectNotFound", {});
+});
